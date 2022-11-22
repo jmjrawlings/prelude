1feb9b3c6c5d22192410b37f4221665be25777b9
Justin Rawlings <hrkn@pm.me>
2022-11-22 23:03:16 +0000

    xd

--- scripts/sync-requirements.sh
+++ scripts/sync-requirements.sh
@@ -11,7 +11,7 @@ do
   input_file="$REQ/requirements-$ENV.in"
   output_file="$REQ/requirements-$ENV.txt"
   
-  echo -n "Compiling $ENV requirements ..."
+  echo -n "Compiling $ENV requirements ... "
     
   pip-compile \
     --resolver=backtracking  \
@@ -23,5 +23,12 @@ do
   count=$(grep -c "^[a-Z]" ${output_file})
   elapsed="$(( end_time - start_time ))"
     
-  echo "\r${count} packages in ${elapsed}s"
+  echo "${count} packages in ${elapsed}s"
 done
+
+start_time=$SECONDS
+echo "Syncing environment ... "
+pip-sync "$REQ/requirements-dev.txt"
+end_time=$SECONDS
+elapsed="$(( end_time - start_time ))"
+echo "Syncing environment took ${elapsed}s"
